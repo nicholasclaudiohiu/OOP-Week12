2584fbfccf5a7678f301120667368f8140ec3d21
Nicholas Claudio <nicholasclaudiohiu@gmail.com>
2025-11-26 17:34:10 +0700

    Write File myfile.txt

---
+++ src/week12/nicholas/id/ac/umn/file_create.java
@@ -0,0 +1,22 @@
+package week12.nicholas.id.ac.umn;
+
+import java.io.File;
+import java.io.IOException;
+
+public class file_create {
+	public static void main(String[] args) {
+		try {
+			File Obj = new File("myfile.txt");
+			if (Obj.createNewFile()) {
+				System.out.println("File created: " + Obj.getName());
+			}
+			else {
+				System.out.println("File already exists.");
+			}
+		}
+		catch (IOException e) {
+			System.out.println("An error has occurred");
+			e.printStackTrace();
+		}
+	}
+}
