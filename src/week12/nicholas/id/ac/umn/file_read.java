bfc6382a89972913004c0225de1a5c13120d508b
Nicholas Claudio <nicholasclaudiohiu@gmail.com>
2025-11-26 17:35:29 +0700

    Read myfile.txt

---
+++ src/week12/nicholas/id/ac/umn/file_read.java
@@ -0,0 +1,23 @@
+package week12.nicholas.id.ac.umn;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class file_read {
+	public static void main(String[] args) {
+		try {
+			File Obj = new File("myfile.txt");
+			Scanner Reader = new Scanner(Obj);
+			while (Reader.hasNextLine()) {
+				String data = Reader.nextLine();
+				System.out.println(data);
+			}
+			Reader.close();
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("An error has occurred.");
+			e.printStackTrace();
+		}
+	}
+}
