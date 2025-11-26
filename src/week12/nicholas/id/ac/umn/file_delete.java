2153ca30c4551fe5454081df9c1e885985b5a74a
Nicholas Claudio <nicholasclaudiohiu@gmail.com>
2025-11-26 17:38:36 +0700

    Delete myfile.txt

---
+++ src/week12/nicholas/id/ac/umn/file_delete.java
@@ -0,0 +1,17 @@
+package week12.nicholas.id.ac.umn;
+
+import java.io.File;
+
+public class file_delete {
+
+	public static void main(String[] args) {
+		File Obj = new File("myfile.txt");
+		if (Obj.delete()) {
+			System.out.println("The deleted file is : " + Obj.getName());
+		}
+		else {
+			System.out.println("Failed in deleting the file.");
+		}
+	}
+
+}
